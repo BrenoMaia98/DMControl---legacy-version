1dd5f613c990cedf477e34216914e77f6384df57
BrenoMaia98 <brenomaia98@gmail.com>
2020-07-15 01:13:24 -0300

    created component SearchBar

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
 
-import { Container } from './styles';
+import { Item, Label, Input } from 'native-base';
+import { Container, SearchInput } from './styles';
+import IconButton from '../IconButton';
+import { IconEnum } from '../../Utils/PickIcon/types';
 
-const SearchBar: React.FC = () => {
+interface SearchBarProps {
+  label: string;
+  labelPosition: 'stacked' | 'floating' | 'inlineLabel' | 'fixedLabel';
+  onSearch(value: string): void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  label,
+  onSearch,
+  labelPosition,
+}) => {
+  const [value, setValue] = React.useState<string>('');
   return (
     <Container>
-      <SearchInput />
-         </Container>);
+      <Item
+        stackedLabel={labelPosition === 'stacked' || undefined}
+        floatingLabel={labelPosition === 'floating' || undefined}
+        inlineLabel={labelPosition === 'inlineLabel' || undefined}
+        fixedLabel={labelPosition === 'fixedLabel' || undefined}
+        last
+      >
+        <Label>{label}</Label>
+        <SearchInput
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onChange={({ currentTarget }: any) => {
+            console.log(String(currentTarget.value));
+            setValue(String(currentTarget.value));
+          }}
+        />
+      </Item>
+      <IconButton
+        onPress={() => {
+          onSearch(value);
+        }}
+        defaultIcon={IconEnum.GoSearch}
+      />
+    </Container>
+  );
 };
 
 export default SearchBar;
--- src/Components/SearchBar/styles.ts
+++ src/Components/SearchBar/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
+import { Input } from 'native-base';
+import { ColorPalette } from '../../Constants/ColorPalette';
 
 export const Container = styled.View`
   flex-direction: row;
 `;
 
-export const SearchInput = styled.TextInput`
-  flex-direction: row;
+export const SearchInput = styled(Input)`
+  border: 1px solid ${ColorPalette.gray};
 `;
