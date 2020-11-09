7059a0b128dc24603dd89d8705f4e28b60af6fd2
BrenoMaia98 <brenomaia98@gmail.com>
2020-11-08 22:57:03 -0300

    fixing ap crash

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import SQLite from 'react-native-sqlite-storage';
 import StackNavigation from './routes/StackNavigation';
 
-export default function App() {
+export default function App(): JSX.Element {
+  SQLite.enablePromise(true);
+  SQLite.openDatabase(
+    { name: 'DMControlDB', location: 'default' },
+    () => {
+      console.log('Open DB successfully! ♥');
+    },
+    (e) => {
+      console.error("Open DB was'nt successfully t.t", e);
+    }
+  );
+
   return (
     <NavigationContainer>
       <StackNavigation />
