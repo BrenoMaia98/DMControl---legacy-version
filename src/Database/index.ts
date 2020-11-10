0f22b33ce48c9ade5ebe7034baac42f54da69ec9
BrenoMaia98 <brenomaia98@gmail.com>
2020-11-09 23:04:54 -0300

    switching from bare sqlite, to expo-sqlite. Documentation issues

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -1,7 +1,21 @@
+import * as SQLite from 'expo-sqlite';
+
 export interface FoodDTO {
   foodName: string;
   measure: string;
   volumeMlOrGram: string;
   cho: number;
   kcal: number;
+}
+
+export class DmControlDB {
+  private db: SQLite.Database;
+
+  constructor() {
+    this.db = SQLite.openDatabase('database.DMControl');
+  }
+
+  private getConnection() {
+    return this.db;
+  }
 }
